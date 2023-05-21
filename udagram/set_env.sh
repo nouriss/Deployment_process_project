54909b8255a3cfd1c2acafeca71bbb880e754597
nouriss <nourisso@gmail.com>
2023-05-21 17:25:58 +0200

    update environment variable referencing

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgresPassword
+export POSTGRES_HOST=database-1.cppg6yqqmold.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::randomnourdeploymentproject
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
+export JWT_SECRET=AWS_ACCESS_KEY_ID
 export URL=http://localhost:8100
