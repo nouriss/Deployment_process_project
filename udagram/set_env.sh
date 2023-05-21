1cad84b9de37307927e9b01728fdc16f9b197370
nouriss <nourisso@gmail.com>
2023-05-21 18:33:15 +0200

    update front-end URL

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgresPassword
+export POSTGRES_PASSWORD=RDS_POSTGRES_PASSWORD
 export POSTGRES_HOST=database-1.cppg6yqqmold.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::randomnourdeploymentproject
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=AWS_ACCESS_KEY_ID
-export URL=http://localhost:8100
+export URL=http://randomnourdeploymentproject.s3-website-us-east-1.amazonaws.com:8100
