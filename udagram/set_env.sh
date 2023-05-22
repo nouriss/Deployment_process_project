7a1550d9157875d3367b9e3627b5390c19e4fc8f
nouriss <nourisso@gmail.com>
2023-05-22 05:01:09 +0200

    update set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -6,6 +6,6 @@ export POSTGRES_HOST=database-1.cppg6yqqmold.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mediabucket8888
 export AWS_REGION=us-east-1
-export AWS_PROFILE=udacity_cli
+export AWS_PROFILE=default
 export JWT_SECRET=AWS_ACCESS_KEY_ID
 export URL=http://test3-dev2.us-east-1.elasticbeanstalk.com:8100
