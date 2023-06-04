044c11fe07dc90715c9835e4158fc683ccad229e
nouriss <nourisso@gmail.com>
2023-06-04 14:49:53 +0200

    update env variables

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=RDS_POSTGRES_PASSWORD
-export POSTGRES_HOST=database-1.cppg6yqqmold.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-2.cppg6yqqmold.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mediabucket8888
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=AWS_ACCESS_KEY_ID
-export URL=http://test3-dev2.us-east-1.elasticbeanstalk.com:8100
+export URL=Udagramapi-env.eba-6py7v4pb.us-east-1.elasticbeanstalk.com:8100
